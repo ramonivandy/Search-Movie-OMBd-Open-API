634de679f5106c778710fe7872f7d531620bb9c6
telkomdev-ramon <ramonivandysetiawan@gmail.com>
2021-10-08 14:48:03 +0700

    Add Database Connection

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
+const movieRoute = require('./bin/routes/movie');
 
-app.get('/', (req, res) => {
-  res.send('This service is running properly')
-})
+// parse requests of content-type
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
+// index route
+app.get("/", (req, res) => {
+  res.json({ message: "This server is running properly" });
+});
+
+// set port, listen for requests
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Server is running on http://localhost:${port}.`);
+});
+
+//set route app
+app.use('/movie', movieRoute);
